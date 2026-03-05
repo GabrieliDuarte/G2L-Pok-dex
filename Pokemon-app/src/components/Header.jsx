cdac9522d2ec7a7a0c5e71db64151c5508ef41e2
Luan481 <henrriqueluan973@gmail.com>
2026-03-05 20:42:51 -0300

    Projeto

--- Pokemon-app/src/components/Header.jsx
+++ Pokemon-app/src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
         <img src="\icones\material-symbols--menu-rounded.svg" alt=""/>
             <h1>G2L</h1>
             <input type="text" placeholder={'Pesquisar'}/>
-            <button><img src="\icones\material-symbols--search.svg" alt=""/></button>
         </div>
     )
 }
 
 export default Header
+    
