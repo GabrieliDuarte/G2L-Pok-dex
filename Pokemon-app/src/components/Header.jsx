8514ea8480e9a4e465db67d004648513b52637d1
Luan481 <henrriqueluan973@gmail.com>
2026-03-09 22:00:50 -0300

    Projeto

--- Pokemon-app/src/components/Header.jsx
+++ Pokemon-app/src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 function Header() {
+    const [nome, setNome] = useState("");
+    const [txt, setTxt] = useState('')
+    const [img, setImg] = useState("")
+    async function getPokemon(){
+        const data = await fetch (`https://pokeapi.co/api/v2/pokemon/${nome}`)
+        const res = await data.json()
+
+        setImg(res.sprites.front_default)
+        setTxt(res.name)
+    }
     return (
         <div className={'Cabecalho'}>
         <img src="\icones\material-symbols--menu-rounded.svg" alt=""/>
             <h1>G2L</h1>
-            <input type="text" placeholder={'Pesquisar'}/>
+            <input type="text" placeholder={'Pesquisar'} onChange={(e) => setNome(e.target.value)}/>
+            <button onClick={getPokemon}>oi</button>
+            <p>{txt}</p>
+            <img src={img} alt="" />
         </div>
     )
 }
