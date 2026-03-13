d10bd78ac9ae0f1562befe439c4452a84f5a55dd
Luan481 <henrriqueluan973@gmail.com>
2026-03-12 21:43:37 -0300

    Projeto

--- Pokemon-app/src/App.jsx
+++ Pokemon-app/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import Equipes from './components/Equipes';
+import Pokemons from './components/Pokemons';
 function App() {
   return (
     <>
       <div>
         <Header />
         <Equipes />
+        <Pokemons />
       </div>
     </>
   )
